bf7b31d012097b1d6efb540de49c6dfc98d8aae9
vnetsiou <vnetsiou@gmail.com>
2024-07-05 13:43:14 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,50 @@
+﻿$(function(){
+
+
+    $('.product-item img').on("mouseover",function () {
+        var $src = $(this).attr('data-hover');
+        $(this).fadeTo(200,0.30, function() {
+            $(this).attr("src",$src);
+        }).fadeTo(200,1);
+
+    });
+
+    $('.product-item img').on("mouseout",function () {
+        var $src = $(this).attr('data-src');
+        $(this).fadeTo(100,0.70, function() {
+            $(this).attr("src",$src);
+        }).fadeTo(100,1);
+
+    });
+
+    $(".mini-cart-container").on("click", function(event) {
+        event.stopPropagation();
+    });
+    $('#shopping-hover').on("click",function () {
+        $("#mini-cart").toggle("fade",200);
+    });
+    $(".close-mini-cart").on("click", function(event) {
+        event.stopPropagation();
+        $("#mini-cart").toggle("fade",200);
+    });
+
+    $('#mini-cart').on("click",function (event) {
+        if (event.target === this) {
+            $(this).toggle("fade", 200);
+        }
+    });
+
+
+    $("#toggle-mobile-menu").click(function(){
+        $(".menu").toggle();
+    })
+    $(".menu-item.has-children").click(function(){
+        $(this).find(".submenu").toggle();
+    })
+    
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    
+})
---
+++ assets/js/sliders.js
@@ -0,0 +1,103 @@
+﻿var swiper = new Swiper('.swiper-container.one', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    effect: 'coverflow',
+    centeredSlides: true,
+    slidesPerView: '2',
+    loop:true,
+    coverflowEffect: {
+        rotate: -10,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows: true
+    },
+    breakpoints: {  
+        '0': {
+          slidesPerView: 1.3,
+        },
+        '768': {
+          slidesPerView: 2,
+        }
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    }, autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }, navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+
+
+
+
+var swiper = new Swiper('.swiper-container.three', {
+    effect: 'cube',
+    cubeEffect: {
+        slideShadows: false,
+    },
+    slidesPerView: '1',
+    loop:true,
+    autoplay: true,
+    direction:'horizontal',
+    delay: 3000,
+    speed:2000
+
+});
+var swiper = new Swiper('.swiper-container.four', {
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+      },
+    slidesPerView: '1',
+    loop:true,
+    autoplay: true,
+    direction:'horizontal',
+    delay: 3000,
+    speed:2000
+
+});
+
+
+
+
+
+$('.top-bar-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    autoplay:true,
+
+    slidesToShow: 1,
+  });
+
+  $('.widget-4-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+          }
+        }
+    ]
+  });
+      
+$('.select-dropdown__button').on('click', function(){
+	$('.select-dropdown__list').toggleClass('active');
+});
+$('.select-dropdown__list-item').on('click', function(){
+	var itemValue = $(this).data('value');
+	console.log(itemValue);
+	$('.select-dropdown__button span').text($(this).text()).parent().attr('data-value', itemValue);
+	$('.select-dropdown__list').toggleClass('active');
+});
